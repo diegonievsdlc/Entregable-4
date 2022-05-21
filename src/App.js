42b566a8a7996bbaa19578eaa03c4402bf5aea72
diegonievsdlc <algorapidonose@gmail.com>
2022-05-21 14:36:26 -0500

    ADD: pequeños detalles

--- src/App.js
+++ src/App.js
@@ -1,74 +1,81 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
-import UsersList from './components/UsersList';
-import UsersForm from './components/UsersForm';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import UsersList from "./components/UsersList";
+import UsersForm from "./components/UsersForm";
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [modalForm, setModalForm] = useState(false)
-  const [modalRemove, setModalRemove] = useState(false)
-  const [userDeleted, setUserDeleted] = useState('')
-  const [usernameSelected, setUsernameSelected] = useState(null)
+  const [users, setUsers] = useState([]);
+  const [modalForm, setModalForm] = useState(false);
+  const [modalRemove, setModalRemove] = useState(false);
+  const [userDeleted, setUserDeleted] = useState("");
+  const [usernameSelected, setUsernameSelected] = useState(null);
 
   useEffect(() => {
-    getUsers()
-  }, [])
+    getUsers();
+  }, []);
   const getUsers = () => {
-    axios.get('https://users-crud1.herokuapp.com/users/')
-      .then(res => setUsers(res.data))
-  }
+    axios
+      .get("https://users-crud1.herokuapp.com/users/")
+      .then((res) => setUsers(res.data));
+  };
   const showAndHideModal = () => {
-    setModalForm(!modalForm)
-  }
+    setModalForm(!modalForm);
+  };
   const removeUser = (id, name) => {
-    axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`)
-      .then(() => getUsers())
-    setUserDeleted(name)
-    setModalRemove(!modalRemove)
-    getUsers()
-  }
-  const selectUser = user => {
-    setUsernameSelected(user)
-    setModalForm(!modalForm)
-  }
-  const deselectUser = () => setUsernameSelected(null)
+    axios
+      .delete(`https://users-crud1.herokuapp.com/users/${id}/`)
+      .then(() => getUsers());
+    setUserDeleted(name);
+    setModalRemove(!modalRemove);
+  };
+  const selectUser = (user) => {
+    setUsernameSelected(user);
+    setModalForm(!modalForm);
+  };
+  const deselectUser = () => setUsernameSelected(null);
 
   return (
     <div className="App">
       <nav>
         <h1>Usuarios</h1>
-        <button onClick={showAndHideModal}><i className='bx bx-plus'></i> Crear nuevo usuario</button>
+        <button onClick={showAndHideModal}>
+          <i className="bx bx-plus"></i> Crear nuevo usuario
+        </button>
       </nav>
-      <UsersList users={users} removeUser={removeUser} selectUser={selectUser}/>
-      {
-        modalForm && <UsersForm 
-                        getUsers={getUsers} 
-                        showAndHideModal={showAndHideModal} 
-                        usernameSelected={usernameSelected} 
-                        deselectUser={deselectUser} 
-                        users={users}/>
-      }
-      { 
-        modalRemove && (
-          <div className="modal">
-            <div className="modal-remove">
-              <button 
-                className='btn-close' 
-                onClick={() => setModalRemove(!modalRemove)}>
-                  <i className='bx bx-x'></i>
-              </button>
-              <h1>Eliminar Usuario</h1>
-              <p>El usuario {userDeleted} se ha eliminado</p>
-              <button 
-                className='btn-to-accept' 
-                onClick={() => setModalRemove(!modalRemove)}>
-                  Aceptar
-              </button>
-            </div>
+      <UsersList
+        users={users}
+        removeUser={removeUser}
+        selectUser={selectUser}
+      />
+      {modalForm && (
+        <UsersForm
+          getUsers={getUsers}
+          showAndHideModal={showAndHideModal}
+          usernameSelected={usernameSelected}
+          deselectUser={deselectUser}
+        />
+      )}
+      {modalRemove && (
+        <div className="modal">
+          <div className="modal-remove">
+            <button
+              className="btn-close"
+              onClick={() => setModalRemove(!modalRemove)}
+            >
+              <i className="bx bx-x"></i>
+            </button>
+            <h1>Eliminar Usuario</h1>
+            <p>El usuario <b>{userDeleted}</b> se ha eliminado</p>
+            <button
+              className="btn-to-accept"
+              onClick={() => setModalRemove(!modalRemove)}
+            >
+              Aceptar
+            </button>
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
   );
 }
--- src/components/UsersForm.js
+++ src/components/UsersForm.js
@@ -1,87 +1,134 @@
-import axios from 'axios';
-import { useEffect} from 'react';
-import { useForm } from 'react-hook-form';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-const UsersForm = ({getUsers, showAndHideModal, usernameSelected, deselectUser, users}) => {
-    const {register, handleSubmit, reset} = useForm()
-    const defaultValues = {
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        birthday: ''
+const UsersForm = ({
+  getUsers,
+  showAndHideModal,
+  usernameSelected,
+  deselectUser,
+}) => {
+  const { register, handleSubmit, reset } = useForm();
+  const defaultValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    birthday: "",
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  useEffect(() => {
+    if (usernameSelected !== null) {
+      reset({
+        first_name: usernameSelected.first_name,
+        last_name: usernameSelected.last_name,
+        email: usernameSelected.email,
+        password: usernameSelected.password,
+        birthday: usernameSelected.birthday,
+      });
     }
-    useEffect(() => {
-        if(usernameSelected !== null){
-            reset({
-                first_name: usernameSelected.first_name,
-                last_name: usernameSelected.last_name,
-                email: usernameSelected.email,
-                password: usernameSelected.password,
-                birthday: usernameSelected.birthday
-            })
-        }
-    }, [usernameSelected, reset])
-    const submit = data => {
-        if(usernameSelected === null){
-            axios.post('https://users-crud1.herokuapp.com/users/', data)
-                .then(() => getUsers())
-        }else{
-            putUserEdited(data)
-        }
-        showAndHideModal()
-        deselectUser()
-        reset(defaultValues)
+  }, [usernameSelected, reset]);
+  const submit = (data) => {
+    if (usernameSelected === null) {
+      axios
+        .post("https://users-crud1.herokuapp.com/users/", data)
+        .then(() => getUsers());
+    } else {
+      axios
+        .put(
+          `https://users-crud1.herokuapp.com/users/${usernameSelected.id}/`,
+          data
+        )
+        .then(() => getUsers());
     }
+    showAndHideModal();
+    deselectUser();
+    reset(defaultValues);
+  };
 
-    const putUserEdited = data => {
-        for(let i = 0; i < users.length; i++){
-            if(users[i].email === data.email){
-                axios.put(`https://users-crud1.herokuapp.com/users/${users[i].id}/`, data)
-                    .then(() => getUsers())
-            }
-        }
-    } 
-    
-    return (
-        <div className='users-form modal'>
-            <form onSubmit={handleSubmit(submit)}>
-                <button 
-                    type='button'
-                    className='btn-close'
-                    onClick={() => {
-                        deselectUser()
-                        showAndHideModal()
-                    }}>
-                        <i className='bx bx-x'></i>
-                </button>
-                {
-                    usernameSelected === null ? <h2>Nuevo Usuario</h2> : <h2>Editar Usuario</h2> 
-                }
-                <div>
-                    <label htmlFor='first_name'>Nombre</label>
-                    <input type="text" {...register('first_name')} id='first_name' placeholder='Nombre'/>
-                </div>
-                <div>
-                    <label htmlFor='last_name'>Apellido</label>
-                    <input type="text" {...register('last_name')} id='last_name' placeholder='Apellido'/>
-                </div>
-                <div>
-                    <label htmlFor='email'>Email</label>
-                    <input type="email" {...register('email')} id='email' placeholder='Email'/>
-                </div>
-                <div>
-                    <label htmlFor='password'>Contraseña</label>
-                    <input type="password" {...register('password')} id='password' placeholder='Contraseña'/>
-                </div>
-                <div>
-                    <label htmlFor='birthday'>Fecha de nacimiento</label>
-                    <input type="date" {...register('birthday')} id='birthday' placeholder='MM/DD/YYYY'/>
-                </div>
-                <button className='btn-to-accept'>{usernameSelected === null ? 'Agregar nuevo usuario' : 'Guardar Cambios'}</button>
-            </form>
+  return (
+    <div className="users-form modal">
+      <form onSubmit={handleSubmit(submit)}>
+        <button
+          type="button"
+          className="btn-close"
+          onClick={() => {
+            deselectUser();
+            showAndHideModal();
+          }}
+        >
+          <i className="bx bx-x"></i>
+        </button>
+        {usernameSelected === null ? (
+          <h2>Nuevo Usuario</h2>
+        ) : (
+          <h2>Editar Usuario</h2>
+        )}
+        <div>
+          <label htmlFor="first_name">Nombre</label>
+          <input
+            type="text"
+            {...register("first_name")}
+            id="first_name"
+            placeholder="Nombre"
+            required
+          />
         </div>
-    );
+        <div>
+          <label htmlFor="last_name">Apellido</label>
+          <input
+            type="text"
+            {...register("last_name")}
+            id="last_name"
+            placeholder="Apellido"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email")}
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type={showPassword ? "text" : "password"}
+            {...register("password")}
+            id="password"
+            placeholder="Contraseña"
+            required
+          />
+          <button
+            type="button"
+            className="show-and-hide"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            <i className="bx bx-show"></i>
+          </button>
+        </div>
+        <div>
+          <label htmlFor="birthday">Fecha de nacimiento</label>
+          <input
+            type="date"
+            {...register("birthday")}
+            id="birthday"
+            placeholder="MM/DD/YYYY"
+            required
+          />
+        </div>
+        <button className="btn-to-accept">
+          {usernameSelected === null
+            ? "Agregar nuevo usuario"
+            : "Guardar Cambios"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default UsersForm;
