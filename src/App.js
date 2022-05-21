ec5c119248e873ad744937f2ae29e6c330a8d625
diegonievsdlc <algorapidonose@gmail.com>
2022-05-20 21:23:53 -0500

    ADD: finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import UsersList from './components/UsersList';
+import UsersForm from './components/UsersForm';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [modalForm, setModalForm] = useState(false)
+  const [modalRemove, setModalRemove] = useState(false)
+  const [userDeleted, setUserDeleted] = useState('')
+  const [usernameSelected, setUsernameSelected] = useState(null)
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+  const getUsers = () => {
+    axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res => setUsers(res.data))
+  }
+  const showAndHideModal = () => {
+    setModalForm(!modalForm)
+  }
+  const removeUser = (id, name) => {
+    axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`)
+      .then(() => getUsers())
+    setUserDeleted(name)
+    setModalRemove(!modalRemove)
+    getUsers()
+  }
+  const selectUser = user => {
+    setUsernameSelected(user)
+    setModalForm(!modalForm)
+  }
+  const deselectUser = () => setUsernameSelected(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Usuarios</h1>
+        <button onClick={showAndHideModal}><i className='bx bx-plus'></i> Crear nuevo usuario</button>
+      </nav>
+      <UsersList users={users} removeUser={removeUser} selectUser={selectUser}/>
+      {
+        modalForm && <UsersForm 
+                        getUsers={getUsers} 
+                        showAndHideModal={showAndHideModal} 
+                        usernameSelected={usernameSelected} 
+                        deselectUser={deselectUser} 
+                        users={users}/>
+      }
+      { 
+        modalRemove && (
+          <div className="modal">
+            <div className="modal-remove">
+              <button 
+                className='btn-close' 
+                onClick={() => setModalRemove(!modalRemove)}>
+                  <i className='bx bx-x'></i>
+              </button>
+              <h1>Eliminar Usuario</h1>
+              <p>El usuario {userDeleted} se ha eliminado</p>
+              <button 
+                className='btn-to-accept' 
+                onClick={() => setModalRemove(!modalRemove)}>
+                  Aceptar
+              </button>
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 }
---
+++ src/components/UsersList.js
@@ -0,0 +1,33 @@
+const UsersList = ({users, removeUser, selectUser}) => {
+    return (
+        <div className='user-list'>
+            {
+                users.map(user => (
+                    <div className='user-card' key={user.id}>
+                        <h2>{user.first_name + " " + user.last_name}</h2>
+                        <div className='user-info'>
+                            <h4>CORREO</h4>
+                            <span>{user.email}</span>
+                            <h4>CUMPLEAÑOS</h4>
+                            <span><i className='bx bx-gift' ></i> {user.birthday}</span>
+                        </div>
+                        <div className='btn-card'>
+                            <button 
+                                type='button'
+                                onClick={() => removeUser(user.id, user.first_name + " " + user.last_name)}>
+                                    <i className='bx bx-trash'></i>
+                            </button>
+                            <button 
+                                type="button" 
+                                onClick={() => selectUser(user)}>
+                                    <i className='bx bx-pencil'></i>
+                            </button>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
+
+export default UsersList;
